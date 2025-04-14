58c2473363302fd37158cdb11b07ef25586b57ea
Treesaken <mbilalfatiha62@gmail.com>
2025-04-14 17:42:31 +0700

    tambah folder praktikum 5

--- Praktikum-2/admin/sidebar.php
+++
@@ -1,8 +0,0 @@
-<div class="list-group list-group-flush">
-                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="form_nilai.php">Form Nilai</a>
-                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Shortcuts</a>
-                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Overview</a>
-                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Events</a>
-                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Profile</a>
-                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Status</a>
-</div>
